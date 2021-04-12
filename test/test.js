57575a8c061c15ace7bab0115572c96fc461e1c2
xis <aragaki9527@gmail.com>
2021-04-12 21:39:40 +0800

    add test for another file path without `.\`

--- test/test.js
+++ test/test.js
@@ -9,17 +9,25 @@ let data = { content: "" }
 /**
  * 1. happy path
  */
-data.content = '![alt](./test/test.js \"title\")'
+data.content = '![alt](./test/test.png \"title\")'
 plugin(data)
-if (data.content !== '{% asset_img test.js \'"title" "alt"\' %}')
-    throw "failed"
+if (data.content !== '{% asset_img test.png \'"title" "alt"\' %}')
+    throw "failed."
 
 /**
  * 2. chinese
  */
-data.content = '![描述](./测试/测试.js \"标题\")'
+data.content = '![描述](./测试/测试.png \"标题\")'
 plugin(data)
-if (data.content !== '{% asset_img 测试.js \'"标题" "描述"\' %}')
-    throw "failed"
+if (data.content !== '{% asset_img 测试.png \'"标题" "描述"\' %}')
+    throw "failed."
+
+/**
+ * 3. another file path
+ */
+ data.content = '![alt](test/test.png \"title\")'
+ plugin(data)
+ if (data.content !== '{% asset_img test.png \'"title" "alt"\' %}')
+     throw "failed."
 
 console.log("success.")
